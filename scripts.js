df434691707903f6f8e629ce843ea949d468c7fc
olliehaigh <30415793+olliehaigh@users.noreply.github.com>
2020-05-19 08:25:37 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,16 +1,15 @@
-
 // Footer tabs section
 
 function openTab(evt, tabName) {
-    var i, x, tablinks;
-    x = document.getElementsByClassName("tab");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active-blue", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active-blue";
+  var i, x, tablinks;
+  x = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active-blue", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active-blue";
+}
